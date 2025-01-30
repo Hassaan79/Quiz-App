743e274edfc6d50e93c25dcc00aed84f99dd4fbc
Hassaan79 <130352083+Hassaan79@users.noreply.github.com>
2025-01-30 19:27:33 +0500

    changed title

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -57,7 +57,7 @@ const Quiz = () => {
 
   return (
     <div className="container">
-      <h1>Quiz App</h1>
+      <h1>Quiz App with react</h1>
       <hr />
       {result ? <></> : <><h2>
         {[index + 1]}. {question.question}
